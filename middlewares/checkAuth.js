774d377b4d4be2a10709c342d98b80c9ce1ed41f
Pierre-Emmanuel RUF <91452983+Petter-Emmanuel@users.noreply.github.com>
2023-01-09 22:34:15 +0100

    Update checkAuth.js

--- middlewares/checkAuth.js
+++ middlewares/checkAuth.js
@@ -1,2 +1,11 @@
 const jwt = require("jsonwebtoken");
 const SECRET = "jkdejzfrA34GF3Hdhgfxcgv5A3HK5A0";
+const createToken = (user) => {
+  return jwt.sign(
+    {id: user.id,
+    },
+    SECRET,
+    {      expiresIn: "1y",
+      algorithm: "HS256",    }
+  );
+};
