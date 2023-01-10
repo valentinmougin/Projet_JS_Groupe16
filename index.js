ddbfcc6ebe0c73455d94248073dc381e69927c3f
MathieuMarech <94976683+MathieuMarech@users.noreply.github.com>
2023-01-10 16:24:18 +0100

    ajout partie gestion des authorisations

--- index.js
+++ index.js
@@ -4,3 +4,31 @@ const securityRouter = require("./routes/security");
 const helloRouter = require("./routes/hello");
 const checkRequestFormat = require("./middlewares/checkRequestFormat");
 const errorHandler = require("./middlewares/errorHandler");
+
+app.use("/admin", (req, res, next) => {
+        if(!req.headers["content-type"]?.startsWith("application/json")) {
+            res.sendStatus(400);
+            return;
+        }
+    next();
+});
+
+app.use("/visitor", (req, res, next) => {
+    if(req.method === "POST" || req.method === "PUT" || req.method === "GET" || req.method === "DELETE") {
+        if(!req.headers["content-type"]?.startsWith("application/json")) {
+            res.sendStatus(400);
+            return;
+        }
+    }
+    next();
+});
+
+app.use("/seller/:id", (req, res, next) => {
+    if(req.method === "POST" || req.method === "PUT" || req.method === "GET" || req.method === "DELETE") {
+        if(!req.headers["content-type"]?.startsWith("application/json")) {
+            res.sendStatus(400);
+            return;
+        }
+    }
+    next();
+});
