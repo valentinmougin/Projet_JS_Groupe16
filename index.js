e939262d075833fea48c129624cdbaabf43e0aa0
MathieuMarech <94976683+MathieuMarech@users.noreply.github.com>
2023-01-10 17:19:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use("/admin", (req, res, next) => {
 });
 
 app.use("/visitor", (req, res, next) => {
-    if(req.method === "POST" || req.method === "PUT" || req.method === "GET" || req.method === "DELETE") {
+    if(req.method === "POST" || req.method === "PUT" || req.method === "DELETE") {
         if(!req.headers["content-type"]?.startsWith("application/json")) {
             res.sendStatus(400);
             return;
